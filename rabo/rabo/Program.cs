4d122c012ab0b6f7e809e56d65c664f6fbe9fb1d
Formova Anastasia <nastya6477374@gmail.com>
2026-02-04 00:28:15 +0700

    Realizatsiya Middleware

--- rabo/rabo/Program.cs
+++ rabo/rabo/Program.cs
@@ -1,6 +1,7 @@
 using CollegeSchedule.Services;
 using Microsoft.EntityFrameworkCore;
 using rabo.Data;
+using rabo.Middlewares;
 using rabo.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -23,6 +24,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
